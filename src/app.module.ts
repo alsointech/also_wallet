9341bde25ced76c2f91236412f7828ef4f1e2105
Alejo <afrancocedeno@gmail.com>
2023-04-16 21:13:15 -0500

    file update | use types on env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
+// dependencies 
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+// modules
 import { CommonModule } from './common/common.module';
 import { MessagesModule } from './messages/messages.module';
 import { AuthModule } from './auth/auth.module';
 import { CommentsModule } from './comments/comments.module';
+import { environments } from './environments';
+import config from './config';
 
 @Module({
-  imports: [MessagesModule, AuthModule, CommonModule, CommentsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true
+    }),
+    MessagesModule,
+    AuthModule,
+    CommonModule,
+    CommentsModule
+  ],
   // controllers: [CommonController],
   // providers: [],
 })
-export class AppModule {}
+export class AppModule { }
